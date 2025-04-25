e6517154c6f5cde66b5874950c0031a7bc9957c9
Ilgar Mamedov <129844629+igmamedov@users.noreply.github.com>
2025-04-26 00:39:11 +0300

    add sarimax model + fs modules

--- src/feature_selection.py
+++ src/feature_selection.py
@@ -1,16 +1,31 @@
-# feature_selection.py
-# Filter, embedded, wrapper methods for feature selection
-
+import phik
 import pandas as pd
 
-def filter_method(data: pd.DataFrame, target: pd.Series) -> list:
-    # Implement filter method for feature selection
-    pass
+class FeatureSelection:
+    """
+    Отбор признаков
 
-def embedded_method(data: pd.DataFrame, target: pd.Series) -> list:
-    # Implement embedded method for feature selection
-    pass
+    Args:
+        X (pd.DataFrame): предлашаемые признаки
+        y (pd.DataFrame): таргет
+    """
+    def __init__(self, X, y):
+        assert len(X) == len(y), "X and y must be the same length"
 
-def wrapper_method(data: pd.DataFrame, target: pd.Series) -> list:
-    # Implement wrapper method for feature selection
-    pass
+        self.X = X
+        self.y = y
+        self.best_features = None
+
+    def select_best(self, n=20, threshold=0.1):
+        """
+        Args:
+            n (int): макс кол-во фичей
+
+        Returns:
+            top_features (list[str]): список лучших признаков
+        """
+        df = pd.concat([self.y, self.X], axis=1).phik_matrix()
+        features = df[df[self.y.name] >= threshold][self.y.name].sort_values(ascending=False)
+        top_features = list(features[1:n+1].index)
+        self.best_features = top_features
+        return top_features
